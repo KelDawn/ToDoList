97f4d00cd8fbb9cdca25e5d1d08802adc365b52c
Kel <33792313+KelDawn@users.noreply.github.com>
2018-06-26 22:34:07 -0600

    todo.php
    commented out const to test for errors

--- app/todo.php
+++ app/todo.php
@@ -9,7 +9,10 @@ class todo extends Model
 protected $table = 'tasklist';
 protected $fillable = ['task', 'priority', 'complete_by', 'time_completed'];
 
+/*
 const CREATED_AT = 'time_added';
 const UPDATED_AT = 'updated_at';
+*/
+
 }
 ?>
